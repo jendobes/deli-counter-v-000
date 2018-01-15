ec359d121a520ae0600367cb8a13fbc7066b99b2
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 23:39:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    constant = "The line is currently:"
+    constant = "The line is currently: "
     katz_deli.each_with_index {|name, index| constant << "#{index+1}. #{name}"}
     puts constant
   end
