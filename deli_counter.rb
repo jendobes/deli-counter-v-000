b175a13edff4f5d8b824e73c07b5ae37d2f6b404
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 22:13:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    puts "The line is currently: #{katz_deli.each_with_index {|name, index| puts "#{index+1}. #{name}"}}"
+    puts "The line is currently: #{katz_deli.map_with_index {|name, index| puts "#{index+1}. #{name}"}}"
   end
 end
 
