2788c8de018329c98612be69879733e335745075
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 21:48:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    puts "The line is currently: #{katz_deli.join (katz_deli.length + 1)}"
+    puts "The line is currently: #{katz_deli.join (katz_deli.length + 1).to_s}"
   end
 end
 
