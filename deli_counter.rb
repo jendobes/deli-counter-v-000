d80d900d35aa5059ba3bddc335ed701ec0662a1a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 23:35:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    puts "The line is currently: #{number} "
+    puts "The line is currently: #{number}"
   end
 end
 
