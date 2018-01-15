6499d367efbe145618c7ac1bd3f52c774147697f
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 22:44:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,8 @@ end
 def now_serving (katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
+  else katz_deli.length >= 1
+    puts "Currently serving #{katz_deli.first}"
+    katz_deli.delete(katz_deli.first)  
 end
 end
